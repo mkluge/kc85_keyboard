18e032c31bf3085737cc44e353cc4d75099b2b4f
Michael Kluge <vollseil@mailbox.org>
2026-07-28 19:42:57 +0200

    v1 check

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,62 @@
 #include <Arduino.h>
 
-constexpr uint8_t GPIO1_PIN = 1;
-constexpr unsigned long BLINK_DELAY_MS = 500;
+constexpr uint8_t KCDATA_PIN = 1;
 
-void blinkGpio1();
+void put_kc(uint8_t y);
+void send_kc(uint8_t i);
+void bith(void);
+void bitl(void);
+void burst(void); // Burst, ist beim Original so, einfaches Low tut es aber auch
 
-void setup() {
-  pinMode(GPIO1_PIN, OUTPUT);
+// -----Senderoutine zum KC-----
+void put_kc(uint8_t y)
+{
+  send_kc(y);
+  delay(12);
+  send_kc(y);
+} // send twice
+// Byte Ausgabe
+void send_kc(uint8_t i) // send
+{
+  uint8_t z; // Bitzähler
+  for (z = 0; z < 7; z++)
+  {
+    (i & (1 << z)) ? bith() : bitl();
+  } // Einzelbits auswerten
+  burst();
 }
 
-void loop() {
-  blinkGpio1();
+void bith(void)
+{
+  burst();
+  delay(7);
+} // Hi Impulsabstand
+void bitl(void)
+{
+  burst();
+  delay(4);
+} // Low Impulsabstand
+
+void burst(void) // Burst, ist beim Original so, einfaches Low tut es aber auch
+{
+  uint8_t t = 5; // ca 140 uS
+  do
+  {
+    digitalWrite(KCDATA_PIN, LOW);
+    delayMicroseconds(14);
+    digitalWrite(KCDATA_PIN, HIGH);
+    delayMicroseconds(14);
+    t--;
+  } while (t);
 }
 
-void blinkGpio1() {
-  digitalWrite(GPIO1_PIN, HIGH);
-  delay(BLINK_DELAY_MS);
-  digitalWrite(GPIO1_PIN, LOW);
-  delay(BLINK_DELAY_MS);
+void setup()
+{
+  pinMode(KCDATA_PIN, OUTPUT);
+}
+
+void loop()
+{
+  send_kc(85);
+  delay(500);
 }
